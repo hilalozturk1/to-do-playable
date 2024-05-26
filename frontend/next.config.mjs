8ab5c5b225598f1dfc3d822dfd53e54ad70d3978
hilalozturk1 <hlloztrk68@gmail.com>
2024-05-26 13:36:10 +0300

    created .

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config, { dev, isServer }) => {
-    if (dev && !isServer) {
-      config.resolve.alias['react-dom'] = '@hot-loader/react-dom';
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
