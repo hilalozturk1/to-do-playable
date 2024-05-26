b29101191be0925200683adcbaa328f910c78a9d
hilalozturk1 <hlloztrk68@gmail.com>
2024-05-26 15:58:36 +0300

    added update signle todo.

--- backend/controllers/todoController.ts
+++ backend/controllers/todoController.ts
@@ -20,3 +20,16 @@ export const getTodos = async (req: Request, res: Response) => {
     res.status(500).json({ message: err.message });
   }
 };
+
+export const updateTodo = async (req: Request, res: Response) => {
+  const todoId = req.params.todoId as string;
+  try {
+    const updatedTodo = await Todo.findByIdAndUpdate(todoId, req.body, { new: true });
+    if (!updatedTodo) {
+      return res.status(404).json({ message: "Todo not found" });
+    }
+    res.json(updatedTodo);
+  } catch (err: any) {
+    res.status(400).json({ message: err.message });
+  }
+};
