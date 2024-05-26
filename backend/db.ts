46838bbad8c6955c2bc7c982fee68eebdaf5fecc
hilalozturk1 <hlloztrk68@gmail.com>
2024-05-26 14:25:37 +0300

    eslint.

--- backend/db.ts
+++ backend/db.ts
@@ -1,27 +1,25 @@
-import mongoose from 'mongoose';
-import User from './models/user';
-import Todo from './models/todo';
+import mongoose from "mongoose";
+import User from "./models/user";
+import Todo from "./models/todo";
 
 const connectDB = async () => {
   try {
-
-    await mongoose.connect('mongodb://localhost/todo-app', {
+    await mongoose.connect("mongodb://localhost/todo-app", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     } as mongoose.ConnectOptions);
-    
+
     await listUsersAndTodos();
 
     await recreateUserCollection();
 
     await listUsersAndTodos();
 
-    console.log('Connected to MongoDB');
+    console.log("Connected to MongoDB");
   } catch (error) {
-    console.error('Error connecting to MongoDB:', error);
+    console.error("Error connecting to MongoDB:", error);
   }
 };
-
 
 const recreateUserCollection = async () => {
   try {
@@ -31,29 +29,50 @@ const recreateUserCollection = async () => {
       console.log(`${collection.name} collection dropped`);
     }
 
-    await mongoose.connection.db.createCollection('users');
-    await mongoose.connection.db.createCollection('todos');
-    console.log('User and Todo collections recreated');
+    await mongoose.connection.db.createCollection("users");
+    await mongoose.connection.db.createCollection("todos");
+    console.log("User and Todo collections recreated");
 
     await insertSampleUsers();
   } catch (error) {
-    console.error('Error recreating collections:', error);
+    console.error("Error recreating collections:", error);
   }
 };
 
 const insertSampleUsers = async () => {
   try {
     const sampleUsers = [
-      { username: 'user1', email: 'user1@example.com', password: 'password1', id:1 },
-      { username: 'user2', email: 'user2@example.com', password: 'password2', id:2 },
+      { username: "user1", email: "user1@example.com", password: "password1", id: 1 },
+      { username: "user2", email: "user2@example.com", password: "password2", id: 2 },
     ];
 
     const insertedUsers = await User.insertMany(sampleUsers);
 
     const sampleTodos = [
-      { title: 'ToDo 1 for User 1', description: 'Description for ToDo 1', tag: 'completed', image: './uploads/pictures/bod_mainImg_01.jpg', attachment: './upload/bod_mainImg_01.jpg', user: insertedUsers[0].id },
-      { title: 'ToDo 2 for User 1', description: 'Description for ToDo 2', tag: 'ready', image: 'https://www.ricoh-imaging.co.jp/english/products/q-s1/ex/img/bod_mainImg_02.jpg', attachment: 'attachment2.pdf', user: insertedUsers[0].id },
-      { title: 'ToDo 1 for User 2', description: 'Description for ToDo 1', tag: 'completed', image: 'image3.jpg', attachment: 'attachment3.pdf', user: insertedUsers[1].id },
+      {
+        title: "ToDo 1 for User 1",
+        description: "Description for ToDo 1",
+        tag: "completed",
+        image: "./uploads/pictures/bod_mainImg_01.jpg",
+        attachment: "./upload/bod_mainImg_01.jpg",
+        user: insertedUsers[0].id,
+      },
+      {
+        title: "ToDo 2 for User 1",
+        description: "Description for ToDo 2",
+        tag: "ready",
+        image: "https://www.ricoh-imaging.co.jp/english/products/q-s1/ex/img/bod_mainImg_02.jpg",
+        attachment: "attachment2.pdf",
+        user: insertedUsers[0].id,
+      },
+      {
+        title: "ToDo 1 for User 2",
+        description: "Description for ToDo 1",
+        tag: "completed",
+        image: "image3.jpg",
+        attachment: "attachment3.pdf",
+        user: insertedUsers[1].id,
+      },
     ];
 
     const insertedTodos = await Todo.insertMany(sampleTodos);
@@ -65,24 +84,24 @@ const insertSampleUsers = async () => {
       })
     );
 
-    console.log('Sample users and todos inserted');
+    console.log("Sample users and todos inserted");
   } catch (error) {
-    console.error('Error inserting sample users and todos:', error);
+    console.error("Error inserting sample users and todos:", error);
   }
 };
 
 const listUsersAndTodos = async () => {
   try {
-    const users = await User.find().populate('todos');
+    const users = await User.find().populate("todos");
     const todos = await Todo.find();
 
-    console.log('Users:');
+    console.log("Users:");
     console.log(users);
 
-    console.log('Todos:');
+    console.log("Todos:");
     console.log(todos);
   } catch (error) {
-    console.error('Error listing users and todos:', error);
+    console.error("Error listing users and todos:", error);
   }
 };
 
