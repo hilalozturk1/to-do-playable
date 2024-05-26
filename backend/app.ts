c714ed5b121b9d4e4e0b4df22e726504ffd55515
hilalozturk1 <hlloztrk68@gmail.com>
2024-05-26 14:41:34 +0300

    added env variables.

--- backend/app.ts
+++ backend/app.ts
@@ -3,13 +3,12 @@ import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 
-// Middleware olarak CORS kullanımı
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3001'); // İstemci URL'sini buraya yazın
+  res.header('Access-Control-Allow-Origin', process.env.MONGO_URI!);
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
