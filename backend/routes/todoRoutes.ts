ddf3ffcc8184b225eb44acc2f6ea0019ca4cd2a5
hilalozturk1 <hlloztrk68@gmail.com>
2024-05-28 17:49:24 +0300

    fixed.

--- backend/routes/todoRoutes.ts
+++ backend/routes/todoRoutes.ts
@@ -29,7 +29,7 @@ const upload = multer({ storage: storage });
 router.get("/api/todos", getTodos);
 router.get("/api/todos/:userId", getTodo);
 router.put("/api/todos/:todoId", updateTodo);
-router.post("/api/public/file", upload.single("attachment"), uploadFile);
+router.post("/api/public/file", upload.single("file"), uploadFile);
 router.post("/api/public/image", upload.single("image"), uploadImage);
 
 export default router;
