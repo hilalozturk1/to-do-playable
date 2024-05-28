2ec0e54124cdfa0d78f8557ab32eecfb5d6b6e35
hilalozturk1 <hlloztrk68@gmail.com>
2024-05-28 15:06:45 +0300

    added bootstrap.

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import LoginPage from "./login";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -15,7 +16,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
-        <LoginPage/>
+        <LoginPage />
       </main>
     </>
   );
